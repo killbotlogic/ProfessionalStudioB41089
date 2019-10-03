ee3f0675371ae411c87f5b37ffa4ce7097924640
HAMMERSMITH <hammersmith.kbot.co>
2019-10-03 17:02:45 +1000

    yo

--- classes.py
+++ classes.py
@@ -49,6 +49,9 @@ class Node:
         self.name = name
         self.position = pos
         self.routing_table = RoutingTable()
+        self._min_dist = {}
+        self._tracks = None
+        self._nodes = None
 
     def __eq__(self, other):
         return self.position == other.position
@@ -57,8 +60,84 @@ class Node:
         return self.position.__hash__()
 
     def tracks(self):
-        return {x for x in Earth.tracks if x.begins_at == self or x.ends_at == self}
+        if not self._tracks:
+            self._tracks = {x for x in Earth.tracks if x.begins_at == self or x.ends_at == self}
+        return self._tracks
 
+    def nodes(self):
+        if not self._nodes:
+            self._nodes = set()
+            for x in self.tracks():
+                if x.begins_at != self:
+                    self._nodes |= {x.begins_at}
+                    self._min_dist[x.begins_at] = (x.track_length, x.begins_at)
+                if x.ends_at != self:
+                    self._nodes |= {x.ends_at}
+                    self._min_dist[x.ends_at] = (x.track_length, x.ends_at)
+        return self._nodes
+
+    def min_dist(self, dest):
+        if dest in self._min_dist:
+            return self._min_dist[dest]
+        import random
+
+        return 9999,
+
+        # try one hop
+        # for x in self._nodes:
+        #     if x.min_dist(dest):
+        #         wx_dist = self._min_dist[x] + x.min_dist(dest)
+        #         if dest not in self._min_dist or wx_dist <= self.min_dist(dest):
+        #             self._min_dist[dest] = wx_dist
+        #     # else:
+        #     #     # try two hops
+        #     #     for y in x._nodes:
+        #     #         if y.min_dist(dest):
+        #     #             wxy_dist = self._min_dist[x] + x.min_dist(y) + y.min_dist(dest)
+        #     #             xy_dist = x.min_dist(y) + y.min_dist(dest)
+        #     #             if dest not in self._min_dist or wxy_dist <= self.min_dist(dest):
+        #     #                 self._min_dist[dest] = wxy_dist
+        #     #             if dest not in x._min_dist or xy_dist <= x.min_dist(dest):
+        #     #                 x._min_dist[dest] = xy_dist
+        #
+        # if dest in self._min_dist:
+        #     return self._min_dist[dest]
+
+    def __repr__(self):
+        return f'Node {self.name}'
+
+    # def init_min_dist_table(self):
+    #     for dest_node in Earth.nodes:
+    #         min_dist = 99999999
+    #         for x in self.tracks():
+    #             if x.begins_at == dest_node or x.ends_at == dest_node:  # if directly connected
+    #                 if x.track_length <= min_dist:
+    #                     min_dist = x.track_length
+    #                     self.min_distances[dest_node] = x.track_length
+    #         if min_dist != 99999999:
+    #             self.min_distances[dest_node] = min_dist
+    #
+    #     for dest_node in Earth.nodes:
+    #         if dest_node not in self.min_distances:
+    #             pass
+
+
+    # def min_dist(self, dest_node):
+    #     if not self.min_distances:
+    #
+    #         for x in Earth.nodes:
+    #
+    #         min_dist = 99999999
+    #         for x in self.tracks():
+    #             if x.begins_at == dest_node or x.ends_at == dest_node:  # if directly connected
+    #                 if x.track_length <= min_dist:
+    #                     min_dist = x.track_length
+    #                     self.min_distances[dest_node] = x.track_length
+    #
+    #         return min_dist
+    #
+    # def hop(self, dest_node, curr_dest):
+    #     pass
 
 class RoutingTable:
     def __init__(self):
@@ -82,16 +161,20 @@ class Earth:  # static class
     def __init__(self):
         pass
 
-if __name__ == '__main__':
-    na = Node('A', Point(x=0.0, y=0.0))
-    nb = Node('B', Point(x=1.0, y=1.0))
 
-    tb = Node('Bravo', Point(x=2.0, y=2.0))
-    tc = Node('Charlie', Point(x=3.0, y=3.0))
-    td = Node('Dingo', Point(x=4.0, y=4.0))
-    tf = Node('Foxtrot', Point(x=5.0, y=5.0))
-    tt = Node('Tango', Point(x=6.0, y=6.0))
-    tw = Node('Whiskey', Point(x=7.0, y=7.0))
+if __name__ == '__main__':
+
+
+
+    na = Node('A', Point(x=304.0, y=256.0))
+    nb = Node('B', Point(x=539.0, y=365.0))
+
+    tb = Node('Bravo', Point(x=841.0, y=154.0))
+    tc = Node('Charlie', Point(x=204.0, y=526.0))
+    td = Node('Dingo', Point(x=786.0, y=617.0))
+    tf = Node('Foxtrot', Point(x=56.0, y=285.0))
+    tt = Node('Tango', Point(x=89.0, y=66.0))
+    tw = Node('Whiskey', Point(x=249.0, y=64.0))
 
     ra = Track(tc, td, 5)
     rb = Track(td, tb, 5)
@@ -106,9 +189,11 @@ if __name__ == '__main__':
     rh = Track(na, tt, 2)
     ri = Track(na, tw, 2)
 
-    Earth.nodes |= {na, nb, tb, tc, td, tf, tt, tw}
-    Earth.tracks |= {ra, rb, rc, rd, re, rf, rg, rh, ri}
+    Earth.nodes = {na, nb, tb, tc, td, tf, tt, tw}
+    Earth.tracks = {ra, rb, rc, rd, re, rf, rg, rh, ri}
 
+    for x in Earth.nodes:
+        x.nodes()  # initialize all connected nodes
 
     train = Train(track=rd, dest=tb, dist=0.0, direction=-1, name='bob')
 
@@ -117,58 +202,76 @@ if __name__ == '__main__':
     assert fake == tw  # because same location
     assert fake != tt  # because not same location
 
+    # connected tracks
     assert tw.tracks() == {ri}
     assert td.tracks() == {ra, rb, rd}
 
-    assert tt.routing_table.min_dist(tw) == 4
-    assert tt.routing_table.min_dist(tf) == 4
-    assert tt.routing_table.min_dist(tb) == 8
-    assert tt.routing_table.min_dist(tc) == 8
-    assert tt.routing_table.min_dist(td) == 8
+    # connected nodes
+    assert tw.nodes() == {na}
+    assert td.nodes() == {tc, nb, tb}
 
-    assert tt.routing_table.next(tw) == na
-    assert tt.routing_table.next(tf) == na
-    assert tt.routing_table.next(tb) == na
-    assert tt.routing_table.next(tc) == na
-    assert tt.routing_table.next(td) == na
-
-
-
-    assert nb.routing_table.min_dist(tw) == 5
-    assert nb.routing_table.min_dist(tt) == 5
-    assert nb.routing_table.min_dist(tf) == 5
-    assert nb.routing_table.min_dist(tb) == 3
-    assert nb.routing_table.min_dist(tc) == 3
-    assert nb.routing_table.min_dist(td) == 3
-
-    assert nb.routing_table.next(tw) == na
-    assert nb.routing_table.next(tt) == na
-    assert nb.routing_table.next(tf) == na
-    assert nb.routing_table.next(tb) == tb
-    assert nb.routing_table.next(tc) == tc
-    assert nb.routing_table.next(td) == td
-
-    assert nb.routing_table.dist(via=tb, dest=td) == 8
-    assert nb.routing_table.dist(via=tc, dest=td) == 8
-
-    train.go_to(tw)
 
     train.step()
-    assert train.on_track == rd
-    assert train.distance_from_beginning_of_track == 1.0
 
-    train.step()
-    train.step()
-    train.step()
 
-    assert train.on_track == rf
-    assert train.distance_from_beginning_of_track == 2.0
-    train.step()
-    assert train.distance_from_beginning_of_track == 1.0
-    train.step()
-    assert train.distance_from_beginning_of_track == 0.0
-    train.step()
-    assert train.on_track == ri
-    assert train.distance_from_beginning_of_track == 1.0
-    train.step()
-    assert train.distance_from_beginning_of_track == 2.0
+
+
+
+
+
+
+
+
+
+    # assert tt.min_dist(tw) == 4
+    # assert tt.min_dist(tf) == 4
+    # # assert tt.min_dist(tb) == 8
+    # # assert tt.min_dist(tc) == 8
+    # # assert tt.min_dist(td) == 8
+    #
+    # assert tt.routing_table.next(tw) == na
+    # assert tt.routing_table.next(tf) == na
+    # assert tt.routing_table.next(tb) == na
+    # assert tt.routing_table.next(tc) == na
+    # assert tt.routing_table.next(td) == na
+    #
+    #
+    #
+    # assert nb.routing_table.min_dist(tw) == 5
+    # assert nb.routing_table.min_dist(tt) == 5
+    # assert nb.routing_table.min_dist(tf) == 5
+    # assert nb.routing_table.min_dist(tb) == 3
+    # assert nb.routing_table.min_dist(tc) == 3
+    # assert nb.routing_table.min_dist(td) == 3
+    #
+    # assert nb.routing_table.next(tw) == na
+    # assert nb.routing_table.next(tt) == na
+    # assert nb.routing_table.next(tf) == na
+    # assert nb.routing_table.next(tb) == tb
+    # assert nb.routing_table.next(tc) == tc
+    # assert nb.routing_table.next(td) == td
+    #
+    # assert nb.routing_table.dist(via=tb, dest=td) == 8
+    # assert nb.routing_table.dist(via=tc, dest=td) == 8
+    #
+    # train.go_to(tw)
+    #
+    # train.step()
+    # assert train.on_track == rd
+    # assert train.distance_from_beginning_of_track == 1.0
+    #
+    # train.step()
+    # train.step()
+    # train.step()
+    #
+    # assert train.on_track == rf
+    # assert train.distance_from_beginning_of_track == 2.0
+    # train.step()
+    # assert train.distance_from_beginning_of_track == 1.0
+    # train.step()
+    # assert train.distance_from_beginning_of_track == 0.0
+    # train.step()
+    # assert train.on_track == ri
+    # assert train.distance_from_beginning_of_track == 1.0
+    # train.step()
+    # assert train.distance_from_beginning_of_track == 2.0
--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import time
 
-import math
+# import math
 import gym
-from gym import spaces, logger
-from gym.utils import seeding
-import numpy as np
+# from gym import spaces, logger
+# from gym.utils import seeding
 from gym.envs.classic_control import rendering
+
+
+# import torch
+# import torch.nn as nn
+# import torch.nn.functional as F
+# import torch.optim as optim
+import numpy as np
 
 
 class Point:
@@ -13,30 +19,128 @@ class Point:
         self.x = x
         self.y = y
 
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
+    def __hash__(self):
+        return hash((self.x, self.y))
+
+
+class Track:
+    def __init__(self, a, b, track_length):
+        self.begins_at = a
+        self.ends_at = b
+        self.track_length = track_length
+
+    def __eq__(self, other):
+        return self.begins_at == other.begins_at and self.ends_at == other.ends_at
+
+    def __hash__(self):
+        return hash((self.begins_at.__hash__(), self.ends_at.__hash__()))
+
+    def get_delta(self):
+        delta_x = self.ends_at.p.x - self.begins_at.p.x
+        delta_y = self.ends_at.p.y - self.begins_at.p.y
+        return delta_x, delta_y
+
+    def get_angle(self):
+        delta_x = self.ends_at.p.x - self.begins_at.p.x
+        delta_y = self.ends_at.p.y - self.begins_at.p.y
+
+        # np.arctan(-1) / np.pi
+        return np.arctan(delta_y / delta_x)
+
+    def step(self):
+        pass
+
+
+class Train:
+
+    def __init__(self, track, dest, dist, direction, name):
+        self.on_track = track
+        self.destination = dest
+        self.distance_from_beginning_of_track = dist
+        self.direction = direction  # -1 = from end to beginning, 1 = from beginning to end
+        self.name = name
+        self.speed = 0.1
+
+    def go_to(self, station):
+        pass
+
+    def step(self):
+        self.distance_from_beginning_of_track += self.speed
+
+    def pos(self):
+        delta = self.on_track.get_delta()
+        progress = self.distance_from_beginning_of_track / self.on_track.track_length
+
+        x = self.on_track.begins_at.p.x + progress * delta[0]
+        y = self.on_track.begins_at.p.y + progress * delta[1]
+
+        return x, y
+
+    def __eq__(self, other):
+        return self.name == other.name
+
+    def __hash__(self):
+        return self.name.__hash__()
+
+
+class Node:
+    def __init__(self, name, pos):
+        self.name = name
+        self.p = pos
+        self._tracks = None
+        self._nodes = None
+
+    def __eq__(self, other):
+        return self.p == other.position
+
+    def __hash__(self):
+        return self.p.__hash__()
+
+    def tracks(self):
+        if not self._tracks:
+            self._tracks = {x for x in World.tracks if x.begins_at == self or x.ends_at == self}
+        return self._tracks
+
+    def pos(self):
+        return self.p.x, self.p.y
+
+    def __repr__(self):
+        return f'Node {self.name}'
+
+
 class World(gym.Env):
     metadata = {
         'render.modes': ['human', 'rgb_array'],
         'video.frames_per_second': 1
     }
 
+    tracks = set()
+    nodes = set()
+    points = set()
+
     def __init__(self):
+        self.age = 0
         self.viewer = None
         self.state = None
 
         self.done = False
 
-        self.scr_wid = 600
-        self.scr_hgt = 600
-        # todo: put in actual coordinates in points
-        na = Node('A', Point(1.000, 3.35))
-        nb = Node('B', Point(1.000, 3.35))
+        self.scr_wid = 900
+        self.scr_hgt = 700
 
-        tb = Node('Bravo', Point(1.000, 3.35))
-        tc = Node('Charlie', Point(1.000, 3.35))
-        td = Node('Dingo', Point(1.000, 3.35))
-        tf = Node('Foxtrot', Point(1.000, 3.35))
-        tt = Node('Tango', Point(1.000, 3.35))
-        tw = Node('Whiskey', Point(1.000, 3.35))
+
+        na = Node('A', Point(x=304.0, y=256.0))
+        nb = Node('B', Point(x=539.0, y=365.0))
+
+        tb = Node('Bravo', Point(x=841.0, y=154.0))
+        tc = Node('Charlie', Point(x=204.0, y=526.0))
+        td = Node('Dingo', Point(x=786.0, y=617.0))
+        tf = Node('Foxtrot', Point(x=56.0, y=285.0))
+        tt = Node('Tango', Point(x=89.0, y=66.0))
+        tw = Node('Whiskey', Point(x=249.0, y=64.0))
 
         ra = Track(tc, td, 5)
         rb = Track(td, tb, 5)
@@ -51,18 +155,27 @@ class World(gym.Env):
         rh = Track(na, tt, 2)
         ri = Track(na, tw, 2)
 
-        train = Train(track=re, dest=tb, dist=1.0, direction=-1)
+        self.dest_node = nb
 
-        self.nodes = [na, nb]
-
-
-        self.tracks = [
-            ra, rb, rc, rd, re, rf, rg, rh, ri,
-        ]
+        # dest might be superfluous
+        self.train = Train(track=re, dest=nb, dist=1.0, direction=-1, name='bob')
+        self.train_trans = None
+        World.nodes = {na, nb, tb, tc, td, tf, tt, tw}
+        World.tracks = {ra, rb, rc, rd, re, rf, rg, rh, ri}
 
     def step(self, action):
+        if self.done:
+            raise Exception('test is done.')
+        self.train.step()
+        self.age += 1
 
-        return self.state, 1.0, self.done, {}
+        self.done = self.dest_node.pos == self.train.pos()
+
+        if not self.done:
+            reward = 0.0
+        else:
+            reward = 1.0 / self.age
+        return self.state, reward, self.done, {}
 
     def reset(self):
         self.state = None
@@ -70,6 +183,56 @@ class World(gym.Env):
         return self.state
 
     def render(self, mode='human'):
+
+        if self.viewer is None:
+            self.viewer = rendering.Viewer(self.scr_wid, self.scr_hgt)
+
+            for n in World.nodes:
+                pretty = rendering.make_capsule(length=20, width=20)
+                pretty.set_color(1, 0, 0)
+                pretty.add_attr(rendering.Transform(translation=n.pos(), rotation=0.0, scale=(1, 1)))
+                self.viewer.add_geom(pretty)
+
+            for track in World.tracks:
+                line = rendering.Line(track.begins_at.pos(), track.ends_at.pos())
+                line.set_color(0, 0, 0)
+                self.viewer.add_geom(line)
+
+            cartwidth = 90.0
+            cartheight = 30.0
+            lef, rig, top, bot = -cartwidth / 2, cartwidth / 2, cartheight / 2, -cartheight / 2
+
+            train = rendering.FilledPolygon([(lef, bot), (lef, top), (rig, top), (rig, bot)])
+            train.set_color(0, 1, 0)
+            self.train_trans = rendering.Transform()
+            train.add_attr(self.train_trans)
+            self.viewer.add_geom(train)
+
+        #     train_1 = rendering.FilledPolygon([(lef, bot), (lef, top), (rig, top), (rig, bot)])
+        #     train_1.set_color(0, 1, 0)
+        #     self.train_1_trans = rendering.Transform()
+        #     train_1.add_attr(self.train_1_trans)
+        #     self.viewer.add_geom(train_1)
+
+        # if self.state is None:
+        #     return None
+        #
+        # state = self.state
+        #
+        # t0_track = self.tracks[state[0]]
+        # t1_track = self.tracks[state[1]]
+        # t0_track_loc = t0_track['ctr']
+        # t1_track_loc = t1_track['ctr']
+        #
+        self.train_trans.set_rotation(self.train.on_track.get_angle())
+        self.train_trans.set_translation(self.train.pos()[0], self.train.pos()[1])
+
+        # self.train_1_trans.set_rotation(t1_track['rot'])
+        #
+        # self.train_0_trans.set_translation(t0_track_loc[0], t0_track_loc[1])
+        # self.train_1_trans.set_translation(t1_track_loc[0], t1_track_loc[1])
+
+        # self.train_0_trans.set_rotation()
 
         return self.viewer.render(return_rgb_array=mode == 'rgb_array')
 
@@ -79,45 +242,25 @@ class World(gym.Env):
             self.viewer = None
 
 
-class Track:
-    def __init__(self, a, b, track_length):
-        self.begins_at = a
-        self.ends_at = b
-        self.track_length = track_length
 
 
-# class RoutingTable:
-#     def __init__(self):
-#         pass
-
-
-class Train:
-    def __init__(self, track, dest, dist, direction):
-        self.on_track = track
-        self.destination = dest
-        self.distance_from_beginning_of_track = dist
-        self.direction = direction  # -1 = from end to beginning, 1 = from beginning to end
-
-
-class Node:
-    def __init__(self, name, pos):
-        self.name = name
-        self.positon = pos
-
+gym.envs.registration.register(
+    id='world-v0',
+    entry_point='main:World',
+)
 
 if __name__ == '__main__':
     render_fps = 20
 
+    import main
+    import gym
+    import numpy as np
 
-
-    gym.envs.registration.register(
-        id='world-v0',
-        entry_point='main:World',
-    )
     world = gym.make('world-v0')
+
     world.reset()
+    world.render()
     for x in range(100000000):
         world.step(None)
 
         time.sleep(1 / render_fps)
-
